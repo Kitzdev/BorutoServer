978d93c6b0f0a72bef4b4fb19986463ae0a5d192
Kitbert <kitbert77@gmail.com>
2022-07-04 23:20:24 +0700

    Initialize the Ktor server.

---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,25 @@
+package com.example
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.serialization.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.request.*
+import io.ktor.application.*
+import io.ktor.response.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.example.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ configureRouting() }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
+        }
+    }
+}
