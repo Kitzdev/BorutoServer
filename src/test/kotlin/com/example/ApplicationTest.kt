02fd36321deaccccfe52ecc8f40c86adf833002d
Kitbert <kitbert77@gmail.com>
2022-07-09 15:58:22 +0700

    - Create a test for the Search Heroes endpoint. - Create the test for a not existing endpoint.

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -33,25 +33,6 @@ class ApplicationTest {
         }
     }
 
-    private fun calculatePage(page: Int): Map<String, Int?> {
-        var previousPage: Int? = page
-        var nextPage: Int? = page
-        if (page in 1..4) {
-            nextPage = nextPage?.plus(1)
-        }
-        if (page in 2..5) {
-            previousPage = previousPage?.minus(1)
-        }
-        if (page == 1) {
-            previousPage = null
-        }
-        if (page == 5) {
-            nextPage = null
-        }
-
-        return mapOf(PREVIOUS_PAGE_KEY to previousPage, NEXT_PAGE_KEY to nextPage)
-    }
-
     @OptIn(ExperimentalSerializationApi::class)
     @Test
     fun `access all heroes endpoint, query all pages, assert correct information`() {
@@ -139,4 +120,111 @@ class ApplicationTest {
         }
     }
 
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun `access search heroes endpoint, query hero name, assert single hero result`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes/search?name=sas").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status(),
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString()).heroes.size
+                assertEquals(
+                    expected = 1,
+                    actual = actual,
+                )
+            }
+        }
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun `access search heroes endpoint, query hero name, assert multiple heroes result`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes/search?name=sa").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status(),
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString()).heroes.size
+                assertEquals(
+                    expected = 3,
+                    actual = actual,
+                )
+            }
+        }
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun `access search heroes endpoint, query an empty text, assert empty list as a result`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes/search?name=").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status(),
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString()).heroes
+                assertEquals(
+                    expected = emptyList(),
+                    actual = actual,
+                )
+            }
+        }
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun `access search heroes endpoint, query non existing hero, assert empty list as a result`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes/search?name=unknown").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status(),
+                )
+                val actual = Json.decodeFromString<ApiResponse>(response.content.toString()).heroes
+                assertEquals(
+                    expected = emptyList(),
+                    actual = actual,
+                )
+            }
+        }
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun `access not existing endpoint, assert not found`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/unknown").apply {
+                assertEquals(
+                    expected = HttpStatusCode.NotFound,
+                    actual = response.status(),
+                )
+                assertEquals(
+                    expected = "Page Not Found.",
+                    actual = response.content,
+                )
+            }
+        }
+    }
+
+    private fun calculatePage(page: Int): Map<String, Int?> {
+        var previousPage: Int? = page
+        var nextPage: Int? = page
+        if (page in 1..4) {
+            nextPage = nextPage?.plus(1)
+        }
+        if (page in 2..5) {
+            previousPage = previousPage?.minus(1)
+        }
+        if (page == 1) {
+            previousPage = null
+        }
+        if (page == 5) {
+            nextPage = null
+        }
+
+        return mapOf(PREVIOUS_PAGE_KEY to previousPage, NEXT_PAGE_KEY to nextPage)
+    }
 }
